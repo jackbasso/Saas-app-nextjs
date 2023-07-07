248fd06270ea891b3bfbdfbfd113155eb376aea7
jackbasso <jackbasso.r@gmail.com>
2023-07-06 23:43:08 -0600

    login page with authentication

--- src/core/layouts/App.js
+++ src/core/layouts/App.js
@@ -3,7 +3,7 @@ import Meta from "../components/Meta";
 import Navbar from "../components/Navbar";
 
 // Hide Navbar after checkout
-const hideNavbarPages = ['/success']
+const hideNavbarPages = ['/success', '/login']
 
 export default function AppLayout({ children }) {
   const router = useRouter()
